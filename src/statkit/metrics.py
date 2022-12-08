8e74be35f54fe31f27e56ab1ce142e327b046934
Hylke C. Donker <hylke.donker@gmail.com>
2022-12-08 17:38:31 +0100

    Added classification report.

--- src/statkit/metrics.py
+++ src/statkit/metrics.py
@@ -1,7 +1,15 @@
 """Classification metrics not part of sci-kit learn."""
 from numpy import array, ndarray
-from pandas import Series
-from sklearn.metrics import roc_curve
+from pandas import DataFrame, Series
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    f1_score,
+    roc_auc_score,
+    roc_curve,
+)
+
+from statkit.non_parametric import bootstrap_score
 
 
 def youden_j_threshold(y_true, y_pred) -> float:
@@ -114,3 +122,62 @@ def specificity(y_true, y_prob, threshold: float = 0.5) -> float:
         threshold: Classify as positive when probability exceeds threshold.
     """
     return 1 - false_positive_rate(y_true, y_prob, threshold)
+
+
+def binary_classification_report(
+    y_true,
+    y_pred_proba,
+    threshold: float = 0.5,
+    n_iterations: int = 1000,
+    random_state=None,
+) -> DataFrame:
+    """Compile performance metrics of a binary classifier.
+
+    Args:
+        y_true: Ground truth labels (0 or 1).
+        y_pred: Predicted probability of the positive class.
+        threshold: Dichotomise probabilities greater or equal to this threshold as positive.
+        n_iterations: Number of bootstrap permutations.
+
+    Returns:
+        A dataframe with the estimated classification metrics (`point`) and 95 %
+        confidence interval (from `lower` to `upper`).
+    """
+    scores = DataFrame(
+        index=[
+            "Accuracy",
+            "Average precision",
+            "$F_1$",
+            "ROC AUC",
+            "Sensitivity",
+            "Specificity",
+        ],
+        columns=["point", "lower", "upper"],
+    )
+
+    kwargs = {
+        "n_iterations": n_iterations,
+        "random_state": random_state,
+    }
+
+    rank_scorers = {
+        "ROC AUC": roc_auc_score,
+        "Average precision": average_precision_score,
+    }
+    for name, scorer in rank_scorers.items():
+        score = bootstrap_score(y_true, y_pred_proba, metric=scorer, **kwargs)
+        scores.loc[name] = dict(score)
+
+    # Metrics that require the probability to be dichotomised.
+    class_scorers = {
+        "$F_1$": f1_score,
+        "Sensitivity": sensitivity,
+        "Specificity": specificity,
+        "Accuracy": accuracy_score,
+    }
+    for name, scorer in class_scorers.items():
+        y_pred = (y_pred_proba >= threshold).astype(int)
+        score = bootstrap_score(y_true, y_pred, metric=scorer, **kwargs)
+        scores.loc[name] = dict(score)
+
+    return scores
